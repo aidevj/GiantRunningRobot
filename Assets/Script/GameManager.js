9e30f5719fa5103db77b8caf72e65d4d6239f45f
Aiden Melendez <itm5305@rit.edu>
2017-02-07 22:26:23 -0500

    Add files via upload

---
+++ Assets/Script/GameManager.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+public class GameManager extends MonoBehaviour{
+    // Prefabs //
+    public var playerPrefab : GameObject;
+
+    
+    // use for initialization 
+    function Start () {
+        // create player
+    }
+
+    function Update () {
+
+    }
+}
---
+++ Assets/Script/playerScript.js
@@ -0,0 +1,66 @@
+﻿// Attributes
+private var r2d;      					    // Required: 2D Rigidbody Component
+private var HP : int;     				    // Integer: HP
+private var grounded : boolean = true;   	// Boolean: Grounded (at ground level and not jumping or flying)
+private var deltaTime : float = 0;			// Float: Time Tracker
+
+public var jumpPower : float;    			// Integer: Jump force multiplyer
+
+/// Start function
+/// Used for initialization
+function Start () {
+	Debug.Log("playerScript Initialized");
+    // Get Component of Rigidbody
+    r2d = GetComponent.<Rigidbody2D>();
+}
+
+/// Update function
+/// Called every frame
+function Update () {
+	//Debug.Log("is updating");
+    // Check is player is dead
+    if (isDead) die();
+
+    // Check player is grounded
+    if (!grounded && r2d.velocity.y == 0) {
+        grounded = true;
+    }
+
+    // Input Management
+    //if (Input.GetKey(KeyCode.Space) && grounded){ //for gliding
+    if (Input.GetKeyDown(KeyCode.Space) && grounded){ //for simple jump
+    	deltaTime = Time.deltaTime;
+        jump();
+    }
+    // Attack
+    if (Input.GetKey(KeyCode.RightShift) || Input.GetKey(KeyCode.LeftShift)) {
+        // TO DO: Attack code here.
+    }
+    if(!grounded && Input.GetKey(KeyCode.Space)){
+		glide();
+	}
+}
+
+//*****************************************************************************************
+// Player action functions
+
+function die(){
+    // TO DO: Death sequence code here.
+}
+
+function jump() {
+    r2d.AddForce(transform.up * jumpPower); 
+    grounded = false;
+}
+
+function glide(){
+	Debug.Log("GLIDING");
+	r2d.AddForce(transform.up * jumpPower/50);
+}
+
+//*****************************************************************************************
+// Helper functions
+
+function isDead() {
+    return HP <= 0 ? true : false;
+}
