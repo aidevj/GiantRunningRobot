9702c960626f49d072394336d4f77af4b4547b18
Aiden Melendez <itm5305@rit.edu>
2017-02-15 17:24:26 -0500

    Add files via upload

--- Assets/Script/backgroundScript.js
+++ Assets/Script/backgroundScript.js
@@ -1,19 +1,22 @@
 ﻿#pragma strict
  private var startPos : float;
  private var WIDTH : float;
+ private var leftBound : float = -5.28f; //arb
+ private var teleportPoint : float = -13.45188;
  //NEED A NUMBER TO BE THE CROSSING LINE FOR BG MOVEMENT
 
 function Start () {
 	startPos = transform.position.x;
-	WIDTH = GetComponent.<MeshFilter>().mesh.bounds.size.x;
+	//WIDTH = GetComponent.<MeshFilter>().mesh.bounds.size.x;
+	WIDTH = GetComponent.<Renderer>().bounds.size.x;
 
 	Debug.Log("startPos = " + startPos);
 	Debug.Log("width = " + WIDTH);
 }
 
 function Update () {
-	transform.position.x-=0.1f;
-	if(transform.position.x < startPos - WIDTH*1.5){
-		transform.position.x = startPos + WIDTH*2;
+	transform.position.x-=0.05f;
+	if(transform.position.x <= leftBound-WIDTH/2){
+		transform.position.x = 19.04293;
 	}
 }
--- Assets/Script/playerScript.js
+++ Assets/Script/playerScript.js
@@ -1,10 +1,13 @@
 ﻿// Attributes
 private var r2d;      					    // Required: 2D Rigidbody Component
 private var HP : int;     				    // Integer: HP
-private var grounded : boolean = true;   	// Boolean: Grounded (at ground level and not jumping or flying)
+private var grounded : boolean = false;   	// Boolean: Grounded (at ground level and not jumping or flying)
+private var jumping : boolean = false;   	// Boolean: Grounded (at ground level and not jumping or flying)
 private var deltaTime : float = 0;			// Float: Time Tracker
 
 public var jumpPower : float;    			// Integer: Jump force multiplyer
+
+public var cube : GameObject;
 
 /// Start function
 /// Used for initialization
@@ -12,33 +15,46 @@ function Start () {
 	//Debug.Log("playerScript Initialized");
     // Get Component of Rigidbody
     r2d = GetComponent.<Rigidbody2D>();
+    cube= GameObject.CreatePrimitive(PrimitiveType.Cube);
 }
 
 /// Update function
 /// Called every frame
 function Update () {
-	
+
+
 	//Debug.Log("grounded=" + grounded);
     // Check is player =is dead
     if (isDead) die();
 
     // Check player is grounded
-    if (!grounded && r2d.velocity.y == 0  && r2d.transform.position.y <= -9.032f) {
+    if (!grounded && r2d.transform.position.y <=2.85f ) { //STATE MACHINES LATER // AIDEN ALSO DO SPAWNS
         grounded = true;
+        jumping = false;
+    }
+
+    if(!grounded && !jumping){
+    	transform.position.y -=0.1f;
     }
 
     // Input Management
-    //if (Input.GetKey(KeyCode.Space) && grounded){ //for gliding
     if (Input.GetKeyDown(KeyCode.Space) && grounded){ //for simple jump
     	deltaTime = Time.deltaTime;
+    	jumping = true;
         jump();
+    }
+    if(Input.GetKeyUp(KeyCode.Space)){
+    jumping = false;
     }
     // Attack
     if (Input.GetKey(KeyCode.RightShift) || Input.GetKey(KeyCode.LeftShift)) {
         // TO DO: Attack code here.
     }
-    if(!grounded && Input.GetKey(KeyCode.Space)){
+    if(Input.GetKey(KeyCode.Space)){
 		glide();
+	}
+    if(Input.GetKeyDown(KeyCode.LeftShift)){
+		attack();
 	}
 }
 
@@ -50,15 +66,24 @@ function die(){
 }
 
 function jump() {
-    r2d.AddForce(transform.up * jumpPower); 
+   // r2d.AddForce(transform.up * jumpPower); 
+   if(transform.position.y <=5.5f){
+   		transform.position.y += 0.5f;
+   }
     grounded = false;
 }
 
 function glide(){
 	Debug.Log("GLIDING");
-	r2d.AddForce(transform.up * jumpPower/60);
+	//r2d.AddForce(transform.up * jumpPower/60);
+	r2d.transform.position.y += 0.2f;
 }
 
+function attack(){
+    //make enemy take damage when close enough
+    Debug.Log("Attacked!");
+
+}
 //*****************************************************************************************
 // Helper functions
 
