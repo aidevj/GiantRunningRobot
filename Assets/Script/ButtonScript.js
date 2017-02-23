0f347ce6770e97fa0d8be0abf754cc35b3ba3dc9
Aiden Melendez <itm5305@rit.edu>
2017-02-23 15:54:49 -0500

    Add files via upload
    main menu working

--- Assets/Script/ButtonScript.js
+++ Assets/Script/ButtonScript.js
@@ -1,9 +1,20 @@
 ﻿#pragma strict
+import UnityEngine.SceneManagement;
+import UnityEngine.UI;
 
-function Start () {
-	
+var btn : Button;
+public var target_scene_name : String;		// String: name of Scene the button will load upon click
+
+function Start() {
+	// Get Button component and create a event handler
+	btn.onClick.AddListener(TaskOnClick);
+
 }
 
-function Update () {
-	
+/// Called by event listener
+/// Loads the target scene non-asynchronously
+function TaskOnClick() {
+	Debug.Log("clicked");
+	// Close all currently loaded scenes and loads new scene
+	//SceneManager.LoadScene(target_scene_name, LoadSceneMode.Single);
 }
---
+++ Assets/Script/MenuScript.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+import UnityEngine.SceneManagement;
+
+function Start () {
+	
+}
+
+/// Change Scene function: called through MenuManager in Button component
+/// Closes all other scenes and opens target scenes by ID
+function ChangeScene (targetSceneID : int) {
+	Debug.Log("ChangeScene called: #" + targetSceneID);
+	SceneManager.LoadScene(targetSceneID, LoadSceneMode.Single);
+
+
+}
